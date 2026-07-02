dc7a64f249c6e279da0d0d2229c9e5f4a59527e8
jelasapico <johnconellcadorna@gmail.com>
2026-07-02 16:34:13 +0800

    get api

---
+++ money.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Money Page</title>
+    </head>
+    <body>
+        <h1>Welcome to the Money Page</h1>
+        <p>This page is all about money.</p>
+    </body>
+</html>npm run dev
